f36b365ea795a486fd269436d51abf3195f5e8c9
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-11-16 17:17:55 -0300

    jsonToCsv process

--- src/controller/jsonToCsv.controller.js
+++ src/controller/jsonToCsv.controller.js
@@ -1,5 +1,18 @@
+import moduleName from 'path
+'
+import {createObjectCsvWriter} from 'csv-writer'
 
-export const jsonToCsv = (json) => {
+export const jsonToCsv = (json) => {  
 
-  return '=== csv ===';
+  const filePath = 
+  const csvWriter = createObjectCsvWriter({
+      path: 'path/to/file.csv',
+      header: [
+        {id: 'name', title: 'NAME'},
+        {id: 'lang', title: 'LANGUAGE'}
+      ]
+  });
+
+
+  return json[0]
 }
