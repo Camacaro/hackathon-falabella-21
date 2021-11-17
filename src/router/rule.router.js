8a57a21d4d93d5b555477ce31330a08622c05bb4
LorenaLevilLl <lorena.levil.ll@gmail.com>
2021-11-17 08:19:24 -0300

    add rule mock

--- src/router/rule.router.js
+++ src/router/rule.router.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { httpResponse } from '../utils/httpResponse';
 import { ruleMiddleware } from '../middleware/rule.middleware';
+import { addRuleMiddleware } from '../middleware/addRule.middleware';
 
 export const ruleRouter = express();
 
@@ -12,8 +13,11 @@ ruleRouter.get(`${prefix}/healthy`, async (req, res) => {
 
 
 ruleRouter.post(`${prefix}/get-all`, async (req, res) => {
-  console.log('prrueba');
   return ruleMiddleware(req, res);
+});
+
+ruleRouter.post(`${prefix}/add-rule`, async (req, res) => {
+  return addRuleMiddleware(req, res);
 });
 
 export default ruleRouter;
