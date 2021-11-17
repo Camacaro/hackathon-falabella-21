d1598a4c3ca8b46efb9971870f07a0a8128b7ec9
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-11-17 11:16:18 -0300

    CORS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors'
 
 import {router} from './router'
 import {config} from './config/config'
 import {dbConnection} from './service/db/mongodb-connection';
 
 const app = express();
+
+app.use( cors() );
 
 app.use( express.json() );
 
